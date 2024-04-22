ed6cc4a1ce5a14cc9aa8e39c442d2a4d4ea70a1a
chetanlohani4321 <chetanlohani310@gmail.com>
2024-04-22 15:00:18 +0530

    changes

--- src/Home/index-two.js
+++ src/Home/index-two.js
@@ -1370,7 +1370,7 @@ const IndexTwo = () => {
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="copy-right-two">
-                                <p><a href="#">Reactheme</a> 2023. All Rights Reserved.</p>
+                                <p><a href="#">Reactheme</a>© Copyright Stesso Technology. All Rights Reserved</p>
                                 <div class="menu">
                                     <ul>
                                         <li><a href="#">Tearms</a></li>
